8abc729b360aabdcde14e716c33380befa13f68e
gun <gungungggun@gmail.com>
2017-12-22 16:42:12 +0900

    作成中

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,108 @@
 <template lang="pug">
   div#app
-    h1 battery
-    p {{ level }}
-    div
-      p(v-if="isPlugged") yes
-      p(v-else) no
-    p count {{ count }}
+    div.normal(v-if="!isPlugged")
+      div(v-if="level >= 50")
+        chara(:pattern="4")
+      div(v-else)
+        chara(:pattern="1")
+
+    div.plug(v-else)
+      div.ok(v-if="!isAnimation")
+        chara(:pattern="2")
+
+      div.action(v-else)
+        chara.front(:pattern="3")
+        div.rotate
+          div.bg
+            ul
+              - for (var i = 0; i < 300; i++)
+                li
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import Chara from './components/Chara'
 export default {
+  components: {
+    Chara,
+  },
   data () {
     return {
+      isAnimation: true,
       level: null,
-      isPlugged: false,
-      count: 0
+      isPlugged: false
     }
-  },
-  components: {
-    HelloWorld
   },
   mounted () {
     document.addEventListener('deviceready', this.onDeviceReady, false)
   },
   methods: {
     onDeviceReady () {
-      cordova.plugins.backgroundMode.enable()
-      cordova.plugins.backgroundMode.on('activate', () => {
-        console.log('activate')
-      })
+      /* cordova.plugins.backgroundMode.enable()
       cordova.plugins.backgroundMode.on('enable', () => {
-        console.log('enable')
         setInterval(this.loop, 1000)
-      })
-      cordova.plugins.backgroundMode.on('disable', () => {
-        console.log('disable')
-      })
-      cordova.plugins.backgroundMode.on('deactivate', () => {
-        console.log('deactivate')
-      })
-      cordova.plugins.backgroundMode.on('failure', () => {
-        console.log('failure')
-      })
+      }) */
       window.addEventListener('batterystatus', this.onBatteryStatus, false)
     },
     onBatteryStatus (status) {
-      console.log('battery')
       this.level = status.level
+      if (!this.isPlugged && status.isPlugged) {
+        this.isAnimation = true
+      }
       this.isPlugged = status.isPlugged
-    },
-    loop () {
-      this.count++
+    }
+    /* loop () {
       if (this.count === 100) cordova.plugins.backgroundMode.moveToForeground()
       if (this.count === 150) cordova.plugins.backgroundMode.moveToBackground()
-    }
+    } */
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+html, body
+  margin 0
+  padding 0
+  width 100vw
+  height 100vh
+  overflow hidden
+@keyframes scroll
+  100%
+    transform translateY(-500vh)
+
+#app
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  .normal
+    width 100vw
+    height 100vh
+  .action
+    width 100vw
+    height 100vh
+    position relative
+    .front
+      z-index 10
+      position absolute
+      top 0
+      left 0
+    .rotate
+      position relative
+      left -100vw
+      width 400vw
+      height 100vh
+      transform rotate(30deg)
+      .bg
+        animation-name scroll
+        animation-duration 10s
+        animation-timing-function linear
+        animation-iteration-count infinite
+        ul
+          margin 0
+          padding 0
+          li
+            width 100%
+            height 20px
+            list-style none
+            background #00f
+            &:nth-child(even)
+              background #ff0
 </style>
---
+++ src/components/Chara.vue
@@ -0,0 +1,93 @@
+<template lang="pug">
+  div.cp-chara
+    div.chara
+      img(:src="imgsrc")
+    div.message
+      p(:class="cls") {{ message }}
+</template>
+
+<script>
+export default {
+  props: {
+    prefix: {
+      type: String,
+      default: 'test'
+    },
+    pattern: {
+      type: Number,
+      default: 1
+    }
+  },
+  data () {
+    return {
+      master: {
+        test: {
+          pattern1: {
+            message: 'じゅうでんしてね',
+            cls: 'blink'
+          },
+          pattern2: {
+            message: 'じゅうでんちゅう'
+          },
+          pattern3: {
+            message: 'げんきになるよ'
+          },
+          pattern4: {
+            message: 'げんきです'
+          }
+        }
+      }
+    }
+  },
+  computed: {
+    cls () {
+      let pattern = 'pattern' + this.pattern
+      return this.master[this.prefix][pattern].cls
+    },
+    message () {
+      let pattern = 'pattern' + this.pattern
+      return this.master[this.prefix][pattern].message
+    },
+    imgsrc () {
+      return '/static/test.png'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@keyframes blink
+  0%
+    opacity 0
+  30%
+    opacity 1
+  50%
+    opacity 1
+  70%
+    opacity 1
+  100%
+    opacity 0
+
+.blink
+  animation-name blink
+  animation-duration 1.5s
+  animation-iteration-count infinite
+  animation-play-state running
+
+.cp-chara
+  width 100vw
+  height 100vh
+  .chara
+    width 100%
+    height 80vh
+    img
+      height 100%
+  .message
+    width 100%
+    height 20vh
+    p
+      text-align center
+      font-size 25px
+      font-weight bold
+      color #BF0000
+</style>
